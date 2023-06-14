7ead9e02a61b137a9926fdbbac65a3d92b1194c5
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-06-14 15:52:18 +0300

    done actions in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def update 
+        @user.update(user_params)
+        if @user.save 
+            redirect_to user_path
+        end
+    end
+
+    private
+
+    def set_user 
+        @user = User.find(params[:id])
+    end
+    
+    def user_params
+        params.require(:user).permit(:avatar)
+
+    end
 end
