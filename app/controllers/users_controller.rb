d8d630a6d435b9d749b8db81a2ebc0d80a90500b
mwaurajr <jamhurifrankiline@gmail.com>
2023-07-04 09:22:52 +0300

    addde bundler audit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+      end
+
     def show 
         @user = User.find(params[:id])
     end
+    
     def update 
         @user = User.find(params[:id])
         @user.update(user_params)
